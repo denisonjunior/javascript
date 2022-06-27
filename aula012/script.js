1e5fc54f6560aaf96078f306c43e27d16eadcf91
denisonjunior <dmattosjunior2013@gmail.com>
2022-06-27 10:55:35 -0300

    exercicio do horario do dia

--- aula012/script.js
+++ aula012/script.js
@@ -1,1 +1,21 @@
-alert ('Olá!')
+function carregar() {
+var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'imagens/imagem-manha.jpeg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+        img.src = 'imagens/imagem-tarde.jpeg'
+        document.body.style.background = '#b9846f'
+    } else {
+        // BOA NOITE!
+        img.src = 'imagens/imagem-noite.jpeg'
+        document.body.style.background = '#515154'
+    }
+}
