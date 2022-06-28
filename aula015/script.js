7b4ba8ce67dbdbaf5228a294f8964f265007dec1
denisonjunior <dmattosjunior2013@gmail.com>
2022-06-27 21:48:07 -0300

    arrumei os tamanhos das fotos e terminei o desafio

--- aula015/script.js
+++ aula015/script.js
@@ -15,31 +15,37 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', 'imagens/crianca-homem-01.png')
+                img.setAttribute('src', 'imagens/crianca-homem-01.jpg')
             } else if (idade < 21) {
                 //Jovem
-                img.setAttribute('src', 'imagens/jovem-homem-01.png')
+                img.setAttribute('src', 'imagens/jovem-homem-01.jpg')
             } else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', 'imagens/adulto-homem-01.png')
+                img.setAttribute('src', 'imagens/adulto-homem-01.jpg')
             } else {
                 //Idoso
-                img.setAttribute('src', 'imagens/idoso-homem-01.png')
+                img.setAttribute('src', 'imagens/idoso-homem-01.jpg')
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'imagens/crianca-mulher-02.jpg')
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'imagens/jovem-mulher-02.jpg')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'imagens/adulto-mulher-02.jpg')
             } else {
                 //Idoso
+                img.setAttribute('src', 'imagens/idoso-mulher-02.jpg')
             }
         }
         res.style.textAlign = 'center'
+        
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-        res.appendChild(img)
+        res.appendChild(img
+            )
     }
 }
